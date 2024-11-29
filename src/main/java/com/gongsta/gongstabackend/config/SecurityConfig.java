64a6ef4e303a1ec1d18c02720bf5a846adc454b9
박준이 <zun_e@zunsmac.local>
2024-11-30 01:32:21 +0900

    swagger show

--- src/main/java/com/gongsta/gongstabackend/config/SecurityConfig.java
+++ src/main/java/com/gongsta/gongstabackend/config/SecurityConfig.java
@@ -5,8 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -19,5 +21,14 @@ public class SecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable() // CSRF 비활성화
+                .authorizeHttpRequests()
+                .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll() // Swagger 관련 경로 허용
+                .anyRequest().authenticated(); // 그 외 경로는 인증 필요
+        return http.build();
     }
 }
--- src/main/java/com/gongsta/gongstabackend/controller/AuthController.java
+++ src/main/java/com/gongsta/gongstabackend/controller/AuthController.java
@@ -4,6 +4,7 @@ package com.gongsta.gongstabackend.controller;
 import com.gongsta.gongstabackend.dto.AuthRequest;
 import com.gongsta.gongstabackend.dto.AuthResponse;
 import com.gongsta.gongstabackend.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +16,7 @@ public class AuthController {
     @Autowired
     private AuthService authService;
 
+    @Operation(summary = "사용자 로그인", description = "아이디와 비밀번호를 사용해 로그인합니다.")
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
         String token = authService.login(request.getUsername(), request.getPassword());
